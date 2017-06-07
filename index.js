f1d9ee742bf76f1df710c3c517fc799186eb5c6f
jgrunewald <jgrunewald@deloitte.com>
2017-06-07 14:38:55 -0400

    updates TSA query

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ function getTSAWaitTime (args) {
 
     type = "http";
 
-    var query = "ap=" + args.body.result.parameters['geo-airport'];
+    var query = "ap=" + args.body.result.parameters['geo-airport.IATA'];
 
     var options = {
       host: "apps.tsa.dhs.gov",
